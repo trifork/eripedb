a709fa4321904f4c727401c9bfcfd13e65b52ab5
Erik Søe Sørensen <ess@trifork.com>
2017-06-30 18:21:51 +0200

    The eripedb server proper. Work in progress.

--- src/eripedb.erl
+++ src/eripedb.erl
@@ -7,6 +7,8 @@
 
 %% API
 -export([start_link/1,
+         lookup/2,
+         sync_lookup/2,
          reload/0]).
 
 %% gen_server callbacks
@@ -14,6 +16,8 @@
          terminate/2, code_change/3]).
 
 -define(SERVER, ?MODULE).
+-define(TABLE_NAME, eripedb_data).
+-define(TEMP_TABLE_NAME, eripedb_temp).
 
 %%%===================================================================
 %%% API
@@ -21,6 +25,14 @@
 
 start_link(DatabaseFile) ->
     gen_server:start_link({local, ?SERVER}, ?MODULE, {DatabaseFile}, []).
+
+lookup(ipv4, {A,B,C,D}) ->
+    async_lookup_with_sync_fallback(ipv4, <<A,B,C,D>>);
+lookup(ipv6, {A,B,C,D, E,F,G,H}) ->
+    async_lookup_with_sync_fallback(ipv6, <<A,B,C,D, E,F,G,H>>).
+
+sync_lookup(Type, IP) when is_atom(Type), is_binary(IP) ->
+    gen_server:gen_call({local, ?SERVER}, {sync_lookup, Type, IP}).
 
 reload() ->
     gen_server:gen_call({local, ?SERVER}, reload, 20*1000).
@@ -30,95 +42,69 @@ reload() ->
 %%%===================================================================
 
 -record(state, {
+          table :: ets:tid(),
+          populated :: boolean()
          }).
 
 %%%===================================================================
 %%% gen_server callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init({DatabaseFile}) ->
-    {ok, #state{}}.
+    {ok, #state{
+            table = ets:new(?TABLE_NAME, [named_table, {keypos,1}]),
+            populated = false
+           }}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
+handle_call({sync_lookup, Type, IP}, _From, State) ->
+    case State of
+        #state{populated=false} ->
+            Reply = {error, no_database};
+        #state{populated=true} ->
+            Reply = do_lookup(Type, IP)
+        end,
+    {reply, Reply, State};
 handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+    %% TODO: Log.
+%    {reply, Reply, State}.
+    {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_cast(_Msg, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
 handle_info(_Info, State) ->
     {noreply, State}.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
 terminate(_Reason, _State) ->
     ok.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
 code_change(_OldVsn, State, _Extra) ->
     {ok, State}.
 
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+async_lookup_with_sync_fallback(Type, IP) when is_atom(Type), is_binary(IP) ->
+    try do_lookup(Type,IP) of
+        {error, inconsistent} ->
+            sync_lookup(Type, IP);
+        Result ->
+            Result
+    catch _:badarg -> % Table does not exist?
+            sync_lookup(Type, IP)
+    end.
+
+do_lookup(Type, IP) when is_atom(Type), is_binary(IP) ->
+    case ets:prev(?TABLE_NAME, {Type, IP, 1000}) of
+        {Type, _, _}=Key ->
+            case ets:lookup(?TABLE_NAME, Key) of
+                [{_Key, Origin}] ->
+                    {ok, Origin};
+                [] ->
+                    {error, inconsistent} % Huh. Unexpected.
+            end;
+        _ ->
+            %% Either '$end_of_table' or something of another class.
+            {error, unknown}
+    end.
+
--- src/eripedb_parser2.erl
+++ src/eripedb_parser2.erl
@@ -9,7 +9,7 @@
 -export([]).
 
 -type address_prefix() :: {Address::binary(), Bits::0..128}.
--type object_callback(X) :: fun((Class:: route|route6, Prefix::address_prefix(), Origin::binary()|undefined, X) -> X).
+-type object_callback(X) :: fun((Name:: {ipv4|ipv6, Prefix::address_prefix()}, Origin::binary()|undefined, X) -> X).
 
 -define(BUFFER_SIZE, (1 bsl 15)). % 32KB
 
@@ -22,7 +22,7 @@
          }).
 
 -spec read/3 :: (iodata(), object_callback(FoldState), FoldState) -> {ok,FoldState} | {error,_}.
-read(FileName, ObjectCallbackFun, FoldState) when is_function(ObjectCallbackFun, 4) ->
+read(FileName, ObjectCallbackFun, FoldState) when is_function(ObjectCallbackFun, 3) ->
     case file:open(FileName, [binary, read, compressed]) of
         {error,_}=Err ->
             Err;
@@ -45,7 +45,7 @@ read(FileName, ObjectCallbackFun, FoldState) when is_function(ObjectCallbackFun,
 %% High level: Read large chunks, splitting at newlines;
 %% Next level: Find "^origin:" | "^route:" | "^route6:"
 
-%% State machine states: none --{has_header,Class,Name}--> |header| --reported--> skipping --separator--> none.
+%% State machine states: none --{has_header,Name}--> |header| --reported--> skipping --separator--> none.
 
 read_loop(State, FoldState) ->
     read_lines(State, <<>>, 0, none, FoldState).
@@ -76,9 +76,9 @@ read_lines(State, Buffer, Pos, SMState, FoldState) ->
 
 handle_line1(State, Line, SMState, FoldState) ->
     case handle_line(Line, SMState) of
-        {report, Class, Name, Origin, NewSMState} ->
+        {report, Name, Origin, NewSMState} ->
             Callback=State#pstate.callback,
-            Foldstate2 = Callback(Class, Name, Origin, FoldState),
+            Foldstate2 = Callback(Name, Origin, FoldState),
             {NewSMState, Foldstate2};
         SMState2 ->
             {SMState2, FoldState}
@@ -88,20 +88,20 @@ handle_line(<<"#", _/binary>>, SMState) ->
     %% Comment - ignore.
     SMState;
 handle_line(<<"route:", PrefixStr/binary>>, none) ->
-    Prefix = parse_address_prefix4(PrefixStr),
-    {has_header, route, Prefix};
+    {IP,PrefixLength} = parse_address_prefix4(PrefixStr),
+    {has_header, {ipv4, IP, PrefixLength}};
 handle_line(<<"route6:", PrefixStr/binary>>, none) ->
-    Prefix = parse_address_prefix6(PrefixStr),
-    {has_header, route6, Prefix};
-handle_line(<<"origin:", Origin0/binary>>, {has_header, Class, Name}) ->
+    {IP,PrefixLength} = parse_address_prefix6(PrefixStr),
+    {has_header, {ipv6, IP, PrefixLength}};
+handle_line(<<"origin:", Origin0/binary>>, {has_header, Name}) ->
     Origin = trim_leading_spaces(Origin0),
     %% Report!
     %% io:format("DB| ~p\n", [{Class, Name, Origin}]),
-    {report, Class, Name, Origin, skipping};
-handle_line(<<"">>, {has_header, Class, Name}) ->
+    {report, Name, Origin, skipping};
+handle_line(<<"">>, {has_header, Name}) ->
     %% Report with origin=undefined!
     %% %% io:format("DB| ~p\n", [{Class, Name, undefined}]),
-    {report, Class, Name, undefined, none};
+    {report, Name, undefined, none};
 handle_line(<<"">>, _) ->
     none;
 handle_line(Line, SMState) ->
@@ -116,30 +116,17 @@ handle_line(Line, SMState) ->
     end,
     SMState.
 
-
-object_name([{<<"route">>, PrefixStr} | _]) ->
-    {route, parse_address_prefix4(PrefixStr)};
-object_name([{<<"route6">>, PrefixStr} | _]) ->
-    {route6, parse_address_prefix6(PrefixStr)};
-object_name([_ | _]) ->
-    unknown_class.
-
-%%% object_field(): Only return first matching attribute.
-object_field(Name, Attrs) ->
-    lists:keyfind(Name, 1, Attrs).
-
-
 parse_address_prefix4(PrefixStr) ->
     [IPStr,LengthStr] = binary:split(trim_leading_spaces(PrefixStr),<<"/">>),
     {ok, {A,B,C,D}} = inet:parse_ipv4_address(binary_to_list(IPStr)),
     Length = binary_to_integer(LengthStr),
-    {ipv4, <<A,B,C,D>>, Length}.
+    {<<A,B,C,D>>, Length}.
 
 parse_address_prefix6(PrefixStr) ->
     [IPStr,LengthStr] = binary:split(trim_leading_spaces(PrefixStr),<<"/">>),
     {ok, {A,B,C,D,E,F,G,H}} = inet:parse_ipv6_address(binary_to_list(IPStr)),
     Length = binary_to_integer(LengthStr),
-    {ipv6, <<A,B,C,D,E,F,G,H>>, Length}.
+    {<<A,B,C,D,E,F,G,H>>, Length}.
 
 trim_leading_spaces(<<" ", Rest/binary>>) ->
     trim_leading_spaces(Rest);
